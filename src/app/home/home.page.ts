8407163969ebd37968bdc2cc0538c251e48badb5
EnriqueOrtiz2001 <enriquotero@gmail.com>
2025-01-22 14:53:39 -0500

    volumen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,6 +9,11 @@ import { Component } from '@angular/core';
 export class HomePage {
   radio = new Audio('https://rr5100.globalhost1.com/8088/;stream'); // URL del stream de radio
   isPlaying = false;
+  volume: number = 50; // Porcentaje de volumen inicial (50%)
+
+  constructor() {
+    this.setVolume(this.volume); // Establecer volumen inicial
+  }
 
   toggleRadio() {
     try {
@@ -22,5 +27,17 @@ export class HomePage {
       console.error('Error al reproducir la radio:', error);
     }
   }
+
+  setVolume(value: any) {
+    const newVolume = Number(value);
+    if (!isNaN(newVolume)) {
+      this.volume = newVolume;
+      this.radio.volume = this.volume / 100; // Convierte de 0-100 a escala 0-1
+    } else {
+      console.error('El valor del volumen no es un número válido:', value);
+    }
   
-  }
+  
+
+    }
+}
