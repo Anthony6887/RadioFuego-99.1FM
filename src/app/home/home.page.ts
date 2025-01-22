a71994d6a46ebe77e621c587ff3d84fff8914218
EnriqueOrtiz2001 <enriquotero@gmail.com>
2025-01-22 13:52:46 -0500

    Radio Funcional

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,20 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  radio = new Audio('https://rr5100.globalhost1.com/8088/;stream'); // URL del stream de radio
+  isPlaying = false;
 
-  constructor() {}
-
-}
+  toggleRadio() {
+    try {
+      if (this.isPlaying) {
+        this.radio.pause();
+      } else {
+        this.radio.play();
+      }
+      this.isPlaying = !this.isPlaying;
+    } catch (error) {
+      console.error('Error al reproducir la radio:', error);
+    }
+  }
+  
+  }
