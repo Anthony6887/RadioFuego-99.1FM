18af3f5c7d00cd2dcdb75964c9eecc3030a34f45
EnriqueOrtiz2001 <enriquotero@gmail.com>
2025-01-27 09:40:49 -0500

    nombre app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Radio',
+  appName: 'Radio Fuego 99.1 FM',
   webDir: 'www'
 };
 
